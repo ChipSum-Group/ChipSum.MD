27ff8419dc1a13e851f24083f894320eea5b52e6
Elton <88137604+EltonYH@users.noreply.github.com>
2022-07-18 09:56:46 +0800

    Calculate SED for MS trajectory

--- Postproc/SED/cal_SED.py
+++ Postproc/SED/cal_SED.py
@@ -6,17 +6,26 @@ import matplotlib.pyplot as plt
 from scipy.fftpack import fft
 import math, sys
 
+###################################################################
+#
+#  Usage: python3 cal_SED.py dev=0/1/2/3 num=10000/20000/30000/..
+#  Note: this script is used for MS trajectory
+#
+#####################################################################
+
 f_name = 'trj.txt'
+dev = int(sys.argv[1].split("=")[1])
+num = int(sys.argv[2].split("=")[1])
 n_cell = 100        #define the number of unit cell in z axis
 k_in_z = 101        #define the number of k points in z axis
 pi = math.pi
 k0 = pi/(12*1e-10)      #定义z轴倒格矢长度
-dt = 5e-15         #取样时间间隔s
-tao0 = 5e-11
+potim = 5e-15         #取样时间间隔s
+tao0 = potim*num
 Fs = (1/dt)*1e-12    #抽样频率THz
 natoms = int(linecache.getline(f_name, 1))
 atoms = int(natoms/n_cell)
-dev = int(sys.argv[1].split("=")[1])
+
 print(dev)
 
 periodic_mass = {'H': 1.00794, 'He': 4.002602, 'Li': 6.941, 'Be': 9.0121831, 'B': 10.811, 'C': 12.0107, 'N': 14.0067, 'O': 15.9994, 'F': 18.99840316, 'Ne': 20.1797, 'Na': 22.98976928, 'Mg': 24.305, 'Al': 26.9815385, 'Si': 28.0855, 'P': 30.973762, 'S': 32.065, 'Cl': 35.453, 'Ar': 39.948, 'K': 39.0983, 'Ca': 40.078, 'Sc': 44.955908, 'Ti': 47.867, 'V': 50.9415, 'Cr': 51.9961, 'Mn': 54.938044, 'Fe': 55.845, 'Co': 58.933194, 'Ni': 58.6934, 'Cu': 63.546, 'Zn': 65.38, 'Ga': 69.723, 'Ge': 72.64, 'As': 74.921595, 'Se': 78.971, 'Br': 79.904, 'Kr': 83.798, 'Rb': 85.4678, 'Sr': 87.62, 'Y': 88.90584, 'Zr': 91.224, 'Nb': 92.90637, 'Mo': 95.95, 'Tc': 98.9072, 'Ru': 101.07, 'Rh': 102.9055, 'Pd': 106.42, 'Ag': 107.8682, 'Cd': 112.414, 'In': 114.818, 'Sn': 118.71, 'Sb': 121.76, 'Te': 127.6, 'I': 126.90447, 'Xe': 131.293, 'Cs': 132.905452, 'Ba': 137.327, 'La': 138.90547, 'Ce': 140.116, 'Pr': 140.90766, 'Nd': 144.242, 'Pm': 144.9, 'Sm': 150.36, 'Eu': 151.964, 'Gd': 157.25, 'Tb': 158.92535, 'Dy': 162.5, 'Ho': 164.93033, 'Er': 167.259, 'Tm': 168.93422, 'Yb': 173.054, 'Lu': 174.9668, 'Hf': 178.49, 'Ta': 180.94788, 'W': 183.84, 'Re': 186.207, 'Os': 190.23, 'Ir': 192.217, 'Pt': 195.084, 'Au': 196.966569, 'Hg': 200.59, 'Tl': 204.3833, 'Pb': 207.2, 'Bi': 208.9804, 'Po': 208.9824, 'At': 209.9871, 'Rn': 222.0176, 'Fr': 223.0197, 'Ra': 226.0245, 'Ac': 227.0277, 'Th': 232.0377, 'Pa': 231.03588, 'U': 238.02891, 'Np': 237.0482, 'Pu': 239.0642, 'Am': 243.0614, 'Cm': 247.0704, 'Bk': 247.0703, 'Cf': 251.0796, 'Es': 252.083, 'Fm': 257.0591, 'Md': 258.0984, 'No': 259.101, 'Lr': 262.1097, 'Rf': 267.1218, 'Db': 268.1257, 'Sg': 269.1286, 'Bh': 274.1436, 'Hs': 277.1519, 'Mt': 278, 'Ds': 281, 'Rg': 282, 'Cn': 285, 'Nh': 284, 'Fl': 289, 'Mc': 288, 'Lv': 292}
@@ -46,43 +55,6 @@ def cal_vel(array1):
         v = (array1[:,i+1]-array1[:,i])*1e-10/dt
         velocity.append(v.tolist())
     return np.array(velocity).T
-
-def cal_acf(lis):
-    N = len(lis)
-    acf = smt.stattools.acf(lis[:], nlags=N-1)
-    return (acf/acf[0]).tolist()
-
-def cal_fft(lis1):
-    n1 = int(math.log(len(lis1),2))
-    if np.power(2,n1) == len(lis1):
-        N = np.power(2,n1)
-        lis3 = lis1[:]
-    else:
-        N = np.power(2,n1+1)
-        n2 = N - len(lis1)
-        lis2 = [0.0]*n2
-        lis3 = lis1[:] + lis2[:]
-    fft_al = fft(lis3)[:int(N/2)]
-    return fft_al.tolist()
-
-def cal_sed(a_vel):
-    fft_final = []
-    for k in range(k_in_z):
-        fft_x, fft_y, fft_z = [], [], []
-        for i in range(n_cell):
-            fft_vx = cal_fft(a_vel[0,i,:].tolist())
-            fft_vy = cal_fft(a_vel[1,i,:].tolist())
-            fft_vz = cal_fft(a_vel[2,i,:].tolist())
-            ek = np.exp(1j*i*k/n_cell/k_in_z)
-            fft_x.append(fft_vx)
-            fft_y.append(fft_vy)
-            fft_z.append((ek*np.array(fft_vz)).tolist())
-        fft_xi = np.average(fft_x[:],axis=0)
-        fft_yi = np.average(fft_y[:],axis=0)
-        fft_zi = np.average(fft_z[:],axis=0)
-        fft_ins = np.sqrt(np.abs(fft_xi)*np.abs(fft_xi)+np.abs(fft_yi)*np.abs(fft_yi)+np.abs(fft_zi)*np.abs(fft_zi))
-        fft_final.append(fft_ins[:].tolist())
-    return fft_final[:]
 
 print('Reading in and dividing trajectory...')
 position, element = read_pos(f_name)
@@ -132,7 +104,6 @@ for i in range(atoms):
     vz_afft = np.array(vz_fftf).T/n_cell
     sed = np.sqrt(np.abs(vx_afft*(vx_afft.conjugate())+vy_afft*(vy_afft.conjugate())+vz_afft*(vz_afft.conjugate())))
     print(np.shape(sed))
-    # sed = cal_sed(np.array([vx[:],vy[:],vz[:]]))
     m = periodic_mass[element[i]]/(6.02e26)
     sed_f.append((np.array(sed[:])*m).tolist())
 sed_final = np.sum(sed_f[:],axis=0)/2/tao0
